945c53e6de02dc2b6628f9567eac39151f497c51
Natan Tibebu <natantibebu111@gmail.com>
2026-06-09 12:46:32 +0300

    Adding some DF and TF

--- app.py
+++ app.py
@@ -5,19 +5,35 @@ with open ("documents.json","r") as file:
 
 query = input("Search query: ").lower() 
 query_words = query.lower().split()
-results = []
+results_tf= []
+results_df = []
+
 for doc in documents:
     doc_words = doc["content"].lower().split()
     tf = 0
     for word in query_words:
         tf += doc_words.count(word)
-    results.append((tf, doc))
+    results_tf.append((tf, doc))
 
 # Sort results by TF-IDF score in descending order
-results.sort(key=lambda x: x[0], reverse=True)
+results_tf.sort(key=lambda x: x[0], reverse=True)
 
-for score, doc in results:
+for score, doc in results_tf:
     print("ID:", doc["id"])
     print("Title:", doc["title"])
     print("Score:", score)
     print("------")
+
+fquery_words = query.lower().split()
+
+for query1 in query_words:
+     df = 0
+     for doc in documents:
+        if query1 in doc["content"].lower().split():
+            df += 1
+     results_df.append((query1,df))
+for i,o in results_df:
+    print(f"DF({i}) = {o}/{len(documents)}")
+    
+
+         
