d8ef324b45c933aac956de2d9dad53221814437c
Natan Tibebu <natantibebu111@gmail.com>
2026-06-10 15:42:37 +0300

    Calculating the df and idf also tf-idf i have miksed the single and quary part i will make it correct

--- app.py
+++ app.py
@@ -1,39 +1,35 @@
 import json
+import math 
 
 with open ("documents.json","r") as file:
     documents = json.load(file)
 
 query = input("Search query: ").lower() 
 query_words = query.lower().split()
+idf = 0
+tf_idf = 0
 results_tf= []
 results_df = []
 
-for doc in documents:
-    doc_words = doc["content"].lower().split()
-    tf = 0
-    for word in query_words:
-        tf += doc_words.count(word)
-    results_tf.append((tf, doc))
-
-# Sort results by TF-IDF score in descending order
-results_tf.sort(key=lambda x: x[0], reverse=True)
-
-for score, doc in results_tf:
-    print("ID:", doc["id"])
-    print("Title:", doc["title"])
-    print("Score:", score)
-    print("------")
-
-fquery_words = query.lower().split()
-
-for query1 in query_words:
+for term in query_words:
      df = 0
      for doc in documents:
-        if query1 in doc["content"].lower().split():
+        if term in doc["content"].lower().split():
             df += 1
-     results_df.append((query1,df))
-for i,o in results_df:
-    print(f"DF({i}) = {o}/{len(documents)}")
+     if df != 0:
+        idf = math.log(len(documents)/df)
+     else:
+         idf = 0
+     
+     for doc in documents:
+         doc_words = doc["content"].lower().split()
+         tf = 0
+         for word in query_words:
+             tf += doc_words.count(word)
+     tf_idf = idf*tf
+     results_df.append((term,df,idf,tf_idf))
+for term,df,idf,tf_idf in results_df:
+    print(f"Word = {term}\nDF({term}) = {df}\nIDF = {idf:.4f}\ntf-idf = {tf_idf}\n\n")
     
 
          
